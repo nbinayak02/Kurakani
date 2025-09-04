db43cf1b09b9461b2c7d546ea402a3c2de538314
nbinayak02 <binayak0128@gmail.com>
2025-09-04 11:35:08 +0545

    Updated send button

--- frontend/src/Components/MessageBubble.jsx
+++ frontend/src/Components/MessageBubble.jsx
@@ -12,7 +12,7 @@ const MessageBubble = (props) => {
   return (
     <div className="m-4 ">
       <div className="flex flex-row items-center">
-        <p className="text-[14px] px-2 text-message-sender-text-light  capitalize">
+        <p className="text-[14px] px-2 text-gray-400 font-semibold capitalize">
           {props.sender}
         </p>
         <p className="text-[11px] font-semibold px-2 text-gray-400">
@@ -20,10 +20,10 @@ const MessageBubble = (props) => {
         </p>
       </div>
       <div
-        className={`h-fit  max-w-full p-2  rounded-xl transition-colors ${
+        className={`h-fit cursor-pointer max-w-full p-2  rounded-xl transition-colors ${
           props.user === props.sender
             ? `text-message-sender-text-light bg-message-sender-light dark:bg-message-sender-dark hover:bg-greenAccentHoverLight dark:hover:bg-greenAccentHover`
-            : `text-message-receiver-text-light dark:text-message-receiver-text-dark bg-message-receiver-light dark:bg-message-receiver-dark hover:bg-gray-600`
+            : `text-message-receiver-text-light dark:text-message-receiver-text-dark bg-gray-300 dark:bg-message-receiver-dark hover:bg-gray-200 dark:hover:bg-gray-600`
         }`}
       >
         {props.msg}
